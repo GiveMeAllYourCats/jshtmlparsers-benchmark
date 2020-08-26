1d6e763509d7c1bb51eaaad37ea41849fc6c2f3e
GiveMeAllYourCats <specamps@gmail.com>
2020-08-27 00:55:30 +0200

    figlet?

--- benchmark.js
+++ benchmark.js
@@ -2,6 +2,7 @@ const BenchmarkJS = require('benchmark')
 const glob = require('glob')
 const async = require('async')
 const path = require('path')
+const figlet = require('figlet')
 const Ora = require('ora')
 const fsp = require('fs').promises
 const fs = require('fs')
@@ -11,6 +12,25 @@ const EventEmitter = require('events')
 const main = async () => {
 	let workersDone = 0
 	let parsers
+	await new Promise((resolve, reject) => {
+		figlet.text(
+			'jshtmlparsers',
+			{
+				font: 'Big',
+				horizontalLayout: 'universal smushing',
+				verticalLayout: 'universal smushing',
+				width: 130,
+				whitespaceBreak: true
+			},
+			function (err, data) {
+				if (err) {
+					return reject(new Error(err))
+				}
+				console.log(data + '\n\n')
+				resolve()
+			}
+		)
+	})
 	let ora = Ora('Starting benchmark').start()
 
 	ora.text = 'Loading html files'
@@ -18,7 +38,7 @@ const main = async () => {
 		return await fsp.readFile(file, 'utf-8')
 	})
 	ora.stop()
-	console.log(`One cycle is ${htmlFiles.length} html files`)
+	console.log(`One cycle is parsing ${htmlFiles.length} html files one by one.`)
 
 	const spawnWorker = parserPath => {
 		const worker = new Worker('./worker.js', {
@@ -55,6 +75,7 @@ const main = async () => {
 	parsers = glob.sync('./parsers/*.js')
 	ora.stop()
 	console.log(`Found ${parsers.length} parsers, spawning equally amount of workers`)
+	console.log(`Each parser will try to create as much cycles possible`)
 	ora.start()
 	ora.text = `Spawning ${parsers.length} workers`
 	parsers.forEach(parserPath => {
